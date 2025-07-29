04feca592ca52be7fb8ab324fefa47f2a1e6a936
YeYuCheng <1104365195@qq.com>
2025-07-29 17:58:34 +0800

    修改部分卡牌

---
+++ src/main/java/patches/MPFlameBarrierCP.java
@@ -0,0 +1,121 @@
+package patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.AbstractCard.CardTarget;
+import com.megacrit.cardcrawl.cards.CardQueueItem;
+import com.megacrit.cardcrawl.cards.red.FlameBarrier;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.input.InputHelper;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.FlameBarrierPower;
+import spireTogether.SpireTogetherMod;
+import spireTogether.monsters.CharacterEntity;
+import spireTogether.network.P2P.P2PManager;
+import spireTogether.network.P2P.P2PPlayer;
+import spireTogether.util.Reflection;
+import java.util.Iterator;
+
+public class MPFlameBarrierCP {
+    public static boolean IsFlameBarrier(AbstractCard o) {
+        if (o == null) {
+            return false;
+        } else {
+            if (o instanceof FlameBarrier){
+                return true;
+            }
+            return false;
+        }
+    }
+
+    private static boolean queueContains(AbstractCard card) {
+        for(CardQueueItem i : AbstractDungeon.actionManager.cardQueue) {
+            if (i.card == card) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @SpirePatch2(
+            clz = AbstractPlayer.class,
+            method = "playCard"
+    )
+    public static class FlameBarrierCP {
+        public static SpireReturn Prefix(AbstractPlayer __instance) {
+            if (SpireTogetherMod.isConnected && MPFlameBarrierCP.IsFlameBarrier(__instance.hoveredCard)) {
+                InputHelper.justClickedLeft = false;
+                __instance.hoverEnemyWaitTimer = 1.0F;
+                __instance.hoveredCard.unhover();
+                if (!MPFlameBarrierCP.queueContains(__instance.hoveredCard)) {
+                    AbstractMonster hoveredMonster = (AbstractMonster)Reflection.getFieldValue("hoveredMonster", __instance);
+                    if (hoveredMonster instanceof CharacterEntity) {
+                        AbstractDungeon.actionManager.cardQueue.add(new CardQueueItem(__instance.hoveredCard, hoveredMonster));
+                    } else {
+                        AbstractDungeon.actionManager.cardQueue.add(new CardQueueItem(__instance.hoveredCard, (AbstractMonster)null));
+                    }
+                }
+
+                Reflection.setFieldValue("isUsingClickDragControl", __instance, false);
+                __instance.hoveredCard = null;
+                __instance.isDraggingCard = false;
+                return SpireReturn.Return();
+            } else {
+                return SpireReturn.Continue();
+            }
+        }
+    }
+
+    @SpirePatch(
+            clz = FlameBarrier.class,
+            method = "use"
+    )
+    public static class FlameBarrierUseCP {
+        public static SpireReturn Prefix(FlameBarrier __instance, AbstractPlayer p, AbstractMonster m) {
+            if (SpireTogetherMod.isConnected && m instanceof CharacterEntity) {
+                m.addBlock(__instance.block);
+                m.addPower(new FlameBarrierPower(m, __instance.magicNumber));
+                return SpireReturn.Return();
+            } else {
+                return SpireReturn.Continue();
+            }
+        }
+    }
+
+    @SpirePatch(
+            clz = AbstractCard.class,
+            method = "update"
+    )
+    public static class TargetPlayersClass {
+        public static void Prefix(AbstractCard __instance) {
+            if (SpireTogetherMod.isConnected && MPFlameBarrierCP.IsFlameBarrier(__instance) && AbstractDungeon.player.isDraggingCard && __instance == AbstractDungeon.player.hoveredCard) {
+                boolean shouldNotReset = false;
+                Iterator pI = P2PManager.GetAllPlayers(false);
+
+                while(pI.hasNext()) {
+                    P2PPlayer next = (P2PPlayer)pI.next();
+                    if (next.IsPlayerInSameRoomAndAction() && next.IsTechnicallyAlive()) {
+                        CharacterEntity entity = next.GetEntity();
+                        if (entity != null && (entity.hb.hovered || next.GetInfobox().IsHovered())) {
+                            __instance.target = CardTarget.ENEMY;
+                            shouldNotReset = true;
+                        }
+                    }
+                }
+
+                if (!shouldNotReset) {
+                    if (AbstractDungeon.player.inSingleTargetMode) {
+                        AbstractDungeon.player.inSingleTargetMode = false;
+                    }
+
+                    __instance.target = CardTarget.SELF;
+                }
+            }
+
+        }
+    }
+}
+
---
+++ src/main/java/patches/MPSentinelCP.java
@@ -0,0 +1,119 @@
+package patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.AbstractCard.CardTarget;
+import com.megacrit.cardcrawl.cards.CardQueueItem;
+import com.megacrit.cardcrawl.cards.red.Sentinel;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.input.InputHelper;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import spireTogether.SpireTogetherMod;
+import spireTogether.monsters.CharacterEntity;
+import spireTogether.network.P2P.P2PManager;
+import spireTogether.network.P2P.P2PPlayer;
+import spireTogether.util.Reflection;
+import java.util.Iterator;
+
+public class MPSentinelCP {
+    public static boolean IsSentinel(AbstractCard o) {
+        if (o == null) {
+            return false;
+        } else {
+            if (o instanceof Sentinel){
+                return true;
+            }
+            return false;
+        }
+    }
+
+    private static boolean queueContains(AbstractCard card) {
+        for(CardQueueItem i : AbstractDungeon.actionManager.cardQueue) {
+            if (i.card == card) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @SpirePatch2(
+            clz = AbstractPlayer.class,
+            method = "playCard"
+    )
+    public static class SentinelCP {
+        public static SpireReturn Prefix(AbstractPlayer __instance) {
+            if (SpireTogetherMod.isConnected && MPSentinelCP.IsSentinel(__instance.hoveredCard)) {
+                InputHelper.justClickedLeft = false;
+                __instance.hoverEnemyWaitTimer = 1.0F;
+                __instance.hoveredCard.unhover();
+                if (!MPSentinelCP.queueContains(__instance.hoveredCard)) {
+                    AbstractMonster hoveredMonster = (AbstractMonster)Reflection.getFieldValue("hoveredMonster", __instance);
+                    if (hoveredMonster instanceof CharacterEntity) {
+                        AbstractDungeon.actionManager.cardQueue.add(new CardQueueItem(__instance.hoveredCard, hoveredMonster));
+                    } else {
+                        AbstractDungeon.actionManager.cardQueue.add(new CardQueueItem(__instance.hoveredCard, (AbstractMonster)null));
+                    }
+                }
+
+                Reflection.setFieldValue("isUsingClickDragControl", __instance, false);
+                __instance.hoveredCard = null;
+                __instance.isDraggingCard = false;
+                return SpireReturn.Return();
+            } else {
+                return SpireReturn.Continue();
+            }
+        }
+    }
+
+    @SpirePatch(
+            clz = Sentinel.class,
+            method = "use"
+    )
+    public static class SentinelUseCP {
+        public static SpireReturn Prefix(Sentinel __instance, AbstractPlayer p, AbstractMonster m) {
+            if (SpireTogetherMod.isConnected && m instanceof CharacterEntity) {
+                m.addBlock(__instance.block);
+                return SpireReturn.Return();
+            } else {
+                return SpireReturn.Continue();
+            }
+        }
+    }
+
+    @SpirePatch(
+            clz = AbstractCard.class,
+            method = "update"
+    )
+    public static class TargetPlayersClass {
+        public static void Prefix(AbstractCard __instance) {
+            if (SpireTogetherMod.isConnected && MPSentinelCP.IsSentinel(__instance) && AbstractDungeon.player.isDraggingCard && __instance == AbstractDungeon.player.hoveredCard) {
+                boolean shouldNotReset = false;
+                Iterator pI = P2PManager.GetAllPlayers(false);
+
+                while(pI.hasNext()) {
+                    P2PPlayer next = (P2PPlayer)pI.next();
+                    if (next.IsPlayerInSameRoomAndAction() && next.IsTechnicallyAlive()) {
+                        CharacterEntity entity = next.GetEntity();
+                        if (entity != null && (entity.hb.hovered || next.GetInfobox().IsHovered())) {
+                            __instance.target = CardTarget.ENEMY;
+                            shouldNotReset = true;
+                        }
+                    }
+                }
+
+                if (!shouldNotReset) {
+                    if (AbstractDungeon.player.inSingleTargetMode) {
+                        AbstractDungeon.player.inSingleTargetMode = false;
+                    }
+
+                    __instance.target = CardTarget.SELF;
+                }
+            }
+
+        }
+    }
+}
+
